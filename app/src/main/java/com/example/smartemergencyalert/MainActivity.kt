82bf0197d8943f6241cbeac48352f0e4b9726b2e
nissy <242ta31210@ashokacollege.in>
2026-03-16 15:18:37 +0530

    Completed Day 1 & 2: UI updates, SOS button, helper classes

--- app/src/main/java/com/example/smartemergencyalert/MainActivity.kt
+++ app/src/main/java/com/example/smartemergencyalert/MainActivity.kt
@@ -1,4 +1,13 @@
 package com.example.smartemergencyalert
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,7 +26,46 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun EmergencyApp() {
-    Text(text = "Smart Emergency Alert System")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "SMART EMERGENCY ALERT",
+            color = Color.White,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(60.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red
+            ),
+            modifier = Modifier
+                .width(200.dp)
+                .height(80.dp)
+        ) {
+
+            Text(
+                text = "SOS",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+        }
+
+    }
 }
